9bc139963e946af19b6cc102fbb205e80db14d82
AlexTasev <aleksandar_tasev@abv.bg>
2019-03-03 14:09:27 +0200

    create-react-app triggered

--- Server-Celera-Chemie/config/routes.js
+++ Server-Celera-Chemie/config/routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const authRoutes = require('../controllers/auth')
+const productRoutes = require('../controllers/product')
 
 const router = new express.Router()
 
-
 module.exports = (app) => {
   app.use('/auth', authRoutes)
+  app.use('/product', productRoutes)
 }
 
 // TODO Check the routs
