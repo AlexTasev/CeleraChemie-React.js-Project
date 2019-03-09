771a2285d66eba22d62a5c494210a0455dfa4d72
AlexTasev <aleksandar_tasev@abv.bg>
2019-03-09 17:46:18 +0200

    Create form completed

--- Client-CeleraChemie/celera-chemie-web-project/src/components/products/CreatePage.jsx
+++ Client-CeleraChemie/celera-chemie-web-project/src/components/products/CreatePage.jsx
@@ -1,9 +1,8 @@
 import React, { Component, Fragment } from "react";
-import { toast } from "react-toastify";
 
 import { createProductValidationFunc } from "../../utils/formValidator";
 import createProductValidator from "../../utils/createProductValidator";
-import Input from '../common/Input'
+import Input from "../common/Input";
 import "./Form.css";
 
 class CreatePage extends Component {
@@ -30,18 +29,28 @@ class CreatePage extends Component {
   onSubmit(e) {
     e.preventDefault();
 
-    let data = (this.state.manufacturer,
-    this.state.description,
-    this.state.category,
-    this.state.logoUrl,
-    this.state.language,
-    this.state.catalogueUrl,
-    this.state.brandWebSite);
-
-    if (!createProductValidator(data)) {
+    if (
+      !createProductValidator(
+        this.state.manufacturer,
+        this.state.description,
+        this.state.category,
+        this.state.logoUrl,
+        this.state.language,
+        this.state.catalogueUrl,
+        this.state.brandWebSite
+      )
+    ) {
       return;
     }
-    this.props.createProduct(data);
+    this.props.createProduct(
+      this.state.manufacturer,
+      this.state.description,
+      this.state.category,
+      this.state.logoUrl,
+      this.state.language,
+      this.state.catalogueUrl,
+      this.state.brandWebSite
+    );
   }
 
   render() {
@@ -60,65 +69,63 @@ class CreatePage extends Component {
         <h1>Create Product</h1>
         <form onSubmit={this.onSubmit}>
           <Input
-            type='text'
-            name='manufacturer'
-            label='Manufacturer'
-            placeholder='Enter manufacturer name'
+            type="text"
+            name="manufacturer"
+            label="Manufacturer"
+            placeholder="Enter manufacturer name"
             value={this.state.manufacturer}
             onChange={this.onChange}
             valid={validObj.validManufacturer}
           />
           <label>Description</label>
           <textarea
-            type='text'
-            name='description'
-            placeholder='Product description'
+            type="text"
+            name="description"
+            placeholder="Product description"
             value={this.state.description}
             onChange={this.onChange}
-            valid={validObj.validDescription}
           />
-          <label>
-            Category
-          <select value={this.state.value} onChange={this.onChange}>
-              <option value="chemicals">Chemicals</option>
-              <option value="consumables">Consumables</option>
-              <option value="instruments">Instruments</option>
-              <option value="glassware">Glassware</option>
-              <option value="filters">Filters</option>
-            </select>
-          </label>
           <Input
-            type='text'
-            name='logoUrl'
-            label='Logo URL'
-            placeholder='Enter manufacturer logo'
+            type="text"
+            name="category"
+            label="Category"
+            placeholder="Enter product category, example: filters"
+            value={this.state.category}
+            onChange={this.onChange}
+            valid={validObj.validCategory}
+          />
+          <Input
+            type="text"
+            name="language"
+            label="Language"
+            placeholder="Enter language, example: en, bg, ro, gr"
+            value={this.state.language}
+            onChange={this.onChange}
+            valid={validObj.validLanguage}
+          />
+          <Input
+            type="text"
+            name="logoUrl"
+            label="Logo URL"
+            placeholder="Enter manufacturer logo"
             value={this.state.logoUrl}
             onChange={this.onChange}
             valid={validObj.validLogoUrl}
           />
-          <label>
-            Language
-          <select value={this.state.value} onChange={this.onChange}>
-              <option value="en">English</option>
-              <option value="bg">Bulgarian</option>
-              <option value="ro">Romanian</option>
-              <option value="gr">Greek</option>
-            </select>
-          </label>
           <Input
-            type='text'
-            name='catalogueUrl'
-            label='Catalogue URL'
-            placeholder='Enter link to catalogue'
+            type="text"
+            name="catalogueUrl"
+            label="Catalogue URL"
+            placeholder="Enter link to catalogue"
             value={this.state.catalogueUrl}
             onChange={this.onChange}
             valid={validObj.validCatalogueUrl}
           />
           <Input
-            type='text'
-            name='brandWebSite'
-            label='Manufacturer web site'
-            placeholder='Enter manufacturer web site'
+            type="text"
+            name="brandWebSite"
+            label="Manufacturer web site"
+            placeholder="Enter manufacturer web site"
             value={this.state.brandWebSite}
             onChange={this.onChange}
             valid={validObj.validBrandUrl}
