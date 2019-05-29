6547cee29f4f6f770da76f47e8805e6d6ef76ca0
unknown <aleksandar_tasev@abv.bg>
2019-05-29 17:16:54 +0300

    css changes

--- Client-CeleraChemie/celera-chemie-web-project/src/components/common/Navigation/Navbar.jsx
+++ Client-CeleraChemie/celera-chemie-web-project/src/components/common/Navigation/Navbar.jsx
@@ -5,7 +5,7 @@ import Translate from "../../../locales/Translate";
 import "../Navigation/Navbar.css";
 
 const Navbar = props => {
-  const { loggedIn, isAdmin, logout,  } = props;
+  const { loggedIn, isAdmin, logout, } = props;
   const userId = localStorage.getItem("userId");
 
   return (
@@ -14,50 +14,52 @@ const Navbar = props => {
         <NavLink className="nav-link-left" to="/">
           <img src={logo} className="logo" alt="Celera-Chemie" />
         </NavLink>
-        {loggedIn && (
-          <NavLink className="nav-link-right" to="/products">
-            <Translate string={"nav.products"} />
+        <div className="navigation-buttons">
+          {loggedIn && (
+            <NavLink className="nav-link-right" to="/products">
+              <Translate string={"nav.products"} />
+            </NavLink>
+          )}
+          {loggedIn && userId && !isAdmin && (
+            <NavLink className="nav-link-right" to={`/user/profile/${userId}`}>
+              <Translate string={"nav.profile"} />
+            </NavLink>
+          )}
+          <NavLink className="nav-link-right" to="/contacts">
+            <Translate string={"nav.contacts"} />
           </NavLink>
-        )}
-        {loggedIn && userId && !isAdmin && (
-          <NavLink className="nav-link-right" to={`/user/profile/${userId}`}>
-            <Translate string={"nav.profile"} />
+          <NavLink className="nav-link-right" to="/about">
+            <Translate string={"nav.about"} />
           </NavLink>
-        )}
-        <NavLink className="nav-link-right" to="/contacts">
-          <Translate string={"nav.contacts"} />
-        </NavLink>
-        <NavLink className="nav-link-right" to="/about">
-          <Translate string={"nav.about"} />
-        </NavLink>
-        <NavLink className="nav-link-right" to="/certificates">
-          <Translate string={"nav.certificates"} />
-        </NavLink>
-        {isAdmin && (
-          <NavLink className="nav-link-right" to="/product/create">
-            <Translate string={"nav.newProduct"} />
+          <NavLink className="nav-link-right" to="/certificates">
+            <Translate string={"nav.certificates"} />
           </NavLink>
-        )}
-        {isAdmin && (
-          <NavLink className="nav-link-right" to="/users">
-            <Translate string={"nav.allUsers"} />
-          </NavLink>
-        )}
-        {!loggedIn && (
-          <NavLink className="nav-link-right" to="/login">
-            <Translate string={"nav.login"} />
-          </NavLink>
-        )}
-        {!loggedIn && (
-          <NavLink className="nav-link-right" to="/register">
-            <Translate string={"nav.register"} />
-          </NavLink>
-        )}
-        {loggedIn && (
-          <a href="/" className="nav-link-right" onClick={logout}>
-            <Translate string={"nav.logout"} />
-          </a>
-        )}
+          {isAdmin && (
+            <NavLink className="nav-link-right" to="/product/create">
+              <Translate string={"nav.newProduct"} />
+            </NavLink>
+          )}
+          {isAdmin && (
+            <NavLink className="nav-link-right" to="/users">
+              <Translate string={"nav.allUsers"} />
+            </NavLink>
+          )}
+          {!loggedIn && (
+            <NavLink className="nav-link-right" to="/login">
+              <Translate string={"nav.login"} />
+            </NavLink>
+          )}
+          {!loggedIn && (
+            <NavLink className="nav-link-right" to="/register">
+              <Translate string={"nav.register"} />
+            </NavLink>
+          )}
+          {loggedIn && (
+            <a href="/" className="nav-link-right" onClick={logout}>
+              <Translate string={"nav.logout"} />
+            </a>
+          )}
+        </div>
       </nav>
     </header>
   );
